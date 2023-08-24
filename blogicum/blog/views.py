ddc4a2c6ab9cec8c0eb21b1c809beb63bda7c6e3
Мартиросян Арман <martirosyan.a2023@yandex.ru>
2023-08-24 16:19:21 +0700

    final

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,8 +5,9 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
-from django.views.generic import CreateView, DeleteView, DetailView, UpdateView
-
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, UpdateView
+)
 from .forms import CommentForm, PostForm, ProfileForm
 from .mixins import CommentRedactMixin, PostListsMixin, PostRedactMixin
 from .models import Category, Comment, Post
@@ -17,24 +18,19 @@ NEW_POSTS = 5
 
 
 class PostListView(PostListsMixin):
-    """Отображение списка постов на главной странице."""
-
     template_name = "blog/index.html"
 
 
 class CategoryListView(PostListsMixin):
-    """Отображение списка постов конкретной категории."""
-
     template_name = "blog/category.html"
 
     def get_queryset(self, *args, **kwargs):
         self.category = get_object_or_404(
             Category, slug=self.kwargs.get("slug"), is_published=True
         )
-        queryset = super().get_queryset(*args, **kwargs).filter(
+        return super().get_queryset(*args, **kwargs).filter(
             category=self.category
         )
-        return queryset
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
@@ -43,8 +39,6 @@ class CategoryListView(PostListsMixin):
 
 
 class ProfileDetailView(PostListsMixin):
-    """Отображение страницы конкретного пользователя со всеми его постами."""
-
     template_name = "blog/profile.html"
 
     def get_queryset(self, *args, **kwargs):
@@ -52,6 +46,10 @@ class ProfileDetailView(PostListsMixin):
             User, username=self.kwargs.get("username")
         )
         if self.user != self.request.user:
+            queryset = super().get_queryset(*args, **kwargs).filter(
+                author=self.user
+            )
+        else:
             queryset = (
                 Post.objects.select_related("author", "category", "location")
                 .order_by("-pub_date")
@@ -67,18 +65,11 @@ class ProfileDetailView(PostListsMixin):
 
 
 class PostDetailView(DetailView):
-    """Отображение отдельного поста."""
-
     model = Post
     template_name = "blog/detail.html"
     pk_url_kwarg = "post_id"
 
     def get_object(self):
-        """
-        Определить автор или не автор делает запрос.
-        Показать любой пост автору и только если опубликован - не автору.
-        """
-
         post = get_object_or_404(Post, id=self.kwargs["post_id"])
         if post.author != self.request.user and (
             not post.is_published or post.pub_date > timezone.now()
@@ -87,11 +78,6 @@ class PostDetailView(DetailView):
         return post
 
     def get_context_data(self, **kwargs):
-        """
-        Запросить все комменты для выбранного поста
-        Дополнительно подгрузить авторов комментариев.
-        """
-
         context = super().get_context_data(**kwargs)
         context["form"] = CommentForm()
         context["comments"] = self.object.comments.select_related("author")
@@ -104,10 +90,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     template_name = "blog/create.html"
 
     def form_valid(self, form):
-        """Присвоить полю author объект пользователя из запроса.
-        Продолжить валидацию, описанную в форме.
-        """
-
         form.instance.author = self.request.user
         return super().form_valid(form)
 
@@ -122,10 +104,6 @@ class PostUpdateView(PostRedactMixin, LoginRequiredMixin, UpdateView):
 
 class PostDeleteView(PostRedactMixin, LoginRequiredMixin, DeleteView):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
     def get_success_url(self):
         username = self.request.user.username
         return reverse("blog:profile", kwargs={"username": username})
@@ -137,10 +115,6 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = "blog/comment.html"
 
     def form_valid(self, form):
-        """Переопределить поля формы,присвоив им значения
-        автора запроса(комментария) и комментируемого поста.
-        """
-
         form.instance.author = self.request.user
         form.instance.post = get_object_or_404(Post, pk=self.kwargs["post_id"])
         return super().form_valid(form)
@@ -152,8 +126,6 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     template_name = "blog/user.html"
 
     def get_object(self):
-        """Получить объект юзера как пользователя из запроса."""
-
         return self.request.user
 
     def get_success_url(self):
@@ -162,12 +134,10 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class CommentUpdateView(LoginRequiredMixin, CommentRedactMixin, UpdateView):
-
     form_class = CommentForm
 
 
 class CommentDeleteView(LoginRequiredMixin, CommentRedactMixin, DeleteView):
 
     def get_success_url(self):
-        """Передать канонический адрес из модели"""
         return self.object.get_absolute_url()
